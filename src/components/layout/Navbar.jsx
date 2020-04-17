1e698677ba750709c6056aee843f5bce63ec1c85
Zachary Ferreira <Pearlybrah@github.com>
2020-04-16 20:56:56 -0700

    added github repos to be fetched using axios, refactored certain components

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  /* 
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
+/* 
   Default props are used in the case that the props being passed arent read 
-  or no longe rvalid, this is the fallback procedure --- MUST be Put above the render
+  or no longer valid, this is the fallback procedure --- MUST be Put above the render in Class based components
+  or below the functional component in Functional Components
   */
-  static defaultProps = {
-    title: "Github-Finder",
-    icon: "fab fa-github",
-  };
-  /*
+Navbar.defaultProps = {
+  title: "Github-Finder",
+  icon: "fab fa-github",
+};
+/*
   PropTypes are used to check the value of the key being passed to its respective TYPE 
   (ie: string, boolean function,  object, etc). Returns error if value passed is not the same as the type required in proptupes object model
   */
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
